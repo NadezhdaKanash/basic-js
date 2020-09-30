03528706918356d591ffad13a2b975e9c1a24695
Nadya Kanash <nadida.hi@gmail.com>
2020-09-30 22:22:23 +0300

    feat: solve the Carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,18 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  let age;
+
+  if(typeof sampleActivity !== 'string') return false;
+  sampleActivity = Number(sampleActivity);
+  if(sampleActivity < 0 || isNaN(sampleActivity)) return false;
+
+  const rate = Math.log(2) / HALF_LIFE_PERIOD;
+  age = Math.ceil(Math.log( MODERN_ACTIVITY / sampleActivity) / rate );
+
+  if(!Number.isInteger(age) || age < 0) return false;
+
+  return age;
 };

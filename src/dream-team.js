0b801486c533e86767185bb63756f522d17e98ab
Nadya Kanash <nadida.hi@gmail.com>
2020-09-30 23:12:48 +0300

    feat: solve Dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members) || members.length == 0) return false;
+  let dreamTeam = members.reduce(function(res, name) {
+    if(typeof name === 'string' && name.length > 0) {
+      return res + name.trim()[0].toUpperCase();
+    } else {
+      return res;
+    }
+  },'');
+  if(dreamTeam.length == 0) return false;
+  return dreamTeam.split('').sort().join('');
 };
